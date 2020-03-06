a41a65ff8540662c5cd0b6d022d357281804a02f
prudhvi138 <59555560+prudhvi138@users.noreply.github.com>
2020-03-06 17:51:26 +0530

    docker6-3-20

---
+++ docker/cts-service/src/main/java/com/example/demo/CtsRestController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CtsRestController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Good morning";
+	}
+	
+	@GetMapping("/languages")
+		public List<String> languages(){
+			
+			return new ArrayList<String>(Arrays.asList("java","python","c","ruby","TypeScript"));
+	}
+
+}
---
+++ docker/prudhvi-cts/src/main/java/com/example/demo/PrudhviCtsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrudhviCtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrudhviCtsApplication.class, args);
+	}
+
+}
